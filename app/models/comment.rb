1cefb796ddce2d01f8c61c19fb7b1e97f5f09f3d
sarahnicoleboo <sarah.cantohyatt.5@my.csun.edu>
2024-11-02 23:21:25 -0700

    validation for comments, now it's done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,32 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  
+  validate :no_election_speak_body
+  
+  def no_election_speak_body
+    if self.body.include?("trump")
+      self.errors.add(:body, "Error: \nBro, you can't post about the election here")
+    end
+    if self.body.include?("Trump")
+      self.errors.add(:body, "Error: \nBro, you can't post about the election here")
+    end
+    if self.body.include?("harris")
+      self.errors.add(:body, "Error: \nBro, you can't post about the election here")
+    end
+    if self.body.include?("Harris")
+      self.errors.add(:body, "Error: \nBro, you can't post about the election here")
+    end
+    if self.body.include?("kamala")
+      self.errors.add(:body, "Error: \nBro, you can't post about the election here")
+    end
+    if self.body.include?("Kamala")
+      self.errors.add(:body, "Error: \nBro, you can't post about the election here")
+    end
+    if self.body.include?("election")
+      self.errors.add(:body, "Error: \nBro, you can't post about the election here")
+    end
+    if self.body.include?("Election")
+      self.errors.add(:body, "Error: \nBro, you can't post about the election here")
+    end
+  end
 end
